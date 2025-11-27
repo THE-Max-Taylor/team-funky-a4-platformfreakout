6bd07fb882ba2a6bd7a1c85af310a2b36c28c073
FrankieMundo-MohawkCollege <frank.mundo3@mohawkcollege.ca>
2025-11-27 17:12:24 -0500

    Made my branch but something broke in program.

---
+++ Assets.cs
@@ -0,0 +1,33 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MohawkGame2D
+{
+
+
+    public class Assets
+    {
+        public Texture2D background1;
+        public void Setup()
+        {
+          
+        }
+
+        public void Update()
+        {
+        
+        Vector2 textureSize = new Vector2(Window.Width,Window.Height);
+        Vector2 position = new Vector2(0, 0);
+        Graphics.DrawSubset(background1, position, position, textureSize);
+        }   
+
+
+
+    }
+}
